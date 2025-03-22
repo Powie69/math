37242926f1f34c7bcc9b3a66ce74235f6b1ce18a
Powie <PowieIsCool@gmail.com>
2025-03-22 12:42:34 +0800

    first prototype

--- x.js
+++ x.js
@@ -1,4 +1,31 @@
-document.getElementById('answer').addEventListener('input', (event) => {
-	event.target.value = event.target.value.replace(/[^0-9]/g, '');
-});
+const subtitle = document.querySelector('.subtitle');
+let answer = 0;
+makeQuestion()
 
+function handleCorrect() {
+	submit.innerText = 'Nice 👌😊';
+	submit.classlist.remove('incorrect');
+	submit.classlist.add('correct');
+}
+
+function handleIncorrect() {
+	submit.innerText = 'WRONG 😡😡😤';
+	submit.classlist.remove('correct');
+	submit.classlist.add('incorrect');
+}
+
+function makeQuestion() {
+	const value1 = Math.floor(Math.random() * 9);
+	const value2 = Math.floor(Math.random() * 9);
+	document.getElementById('firstNumber').textContent = value1;
+	document.getElementById('secondNumber').textContent = value2;
+	answer = value1 + value2;
+}
+
+function submit(guess) {
+	if (guess == answer) {
+		handleCorrect();
+		return makeQuestion();
+	}
+	handleCorrect();
+}
